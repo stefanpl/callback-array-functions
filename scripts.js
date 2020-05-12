d9c39c52529cc9ffdf30ea43bc8da179dd37a30b
Stefan Plösser <stefan@rarelime.de>
2020-05-12 10:45:49 +0200

    inital commit

---
+++ scripts.js
@@ -0,0 +1,70 @@
+const starWarsNames = ['Luke', 'Leia', 'Obi Wan', 'Master Yoda', 'Darth Vader'];
+
+const randomNumbers = [7, 19, 3, 105, 66, 13, 8, 119, 4];
+
+const people = [
+  {
+    firstName: 'Horst',
+    lastName: 'Schlemmer',
+    age: 55,
+    famous: true,
+    gender: 'm',
+  },
+  {
+    firstName: 'Gundula',
+    lastName: 'Gausel',
+    age: 61,
+    famous: true,
+    gender: 'f',
+  },
+  {
+    firstName: 'Some',
+    lastName: 'Dude',
+    age: 25,
+    famous: false,
+    gender: 'm',
+  },
+  {
+    firstName: 'Random',
+    lastName: 'Chick',
+    age: 19,
+    famous: false,
+    gender: 'f',
+  },
+  {
+    firstName: 'Conchita',
+    lastName: 'Wurst',
+    age: 32,
+    famous: true,
+    gender: 'd',
+  },
+  {
+    firstName: 'Karl-Heinz',
+    lastName: 'Ferdenheim',
+    age: 83,
+    famous: false,
+    gender: 'm',
+  },
+  {
+    firstName: 'Marianne',
+    lastName: 'Mosbach',
+    age: 47,
+    famous: false,
+    gender: 'f',
+  },
+  {
+    firstName: 'Michaela',
+    lastName: 'Schaffrath',
+    age: 49,
+    famous: true,
+    gender: 'f',
+  },
+  {
+    firstName: 'Peter',
+    lastName: 'Pan',
+    age: 12,
+    famous: true,
+    gender: 'm',
+  },
+
+];
